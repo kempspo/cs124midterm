36b1eabd083f0c46c924d74d3eb89c631c0db42b
kempspo <kempspo@gmail.com>
2018-04-08 20:30:27 +0800

    Initial commit

---
+++ src/cs124midterm/UI.java
@@ -0,0 +1,23 @@
+package cs124midterm;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.util.*;
+
+public class UI {  
+
+	String output;
+	PrintWriter printer;
+
+	public UI() throws FileNotFoundException
+	{
+		printer = new PrintWriter("log.txt");
+
+		setLayout(new BorderLayout());
+
+		topPanel = new JPanel();
+		topPanel.setLayout()
+	}
+}  
